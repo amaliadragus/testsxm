46c89343340a7c9d00cab8a1c56bc7309b69d171
Amalia Dragus <amalia.dragus@acroots-MacBook-Pro.local>
2020-04-14 21:14:27 +0300

    Added initial files

---
+++ print_date.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Prints the current date
+
+Usage: python3 print_date.py
+
+"""
+
+from datetime import date
+
+
+def main():
+    print(date.today())
+
+
+if __name__ == "__main__":
+    main()
